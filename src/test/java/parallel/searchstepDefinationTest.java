7626e3d04e700e09ddc004d0d06e7b734b6443df
vinay-j-203 <84924516+vinay-j-203@users.noreply.github.com>
2021-07-23 19:43:08 +0530

    Update searchstepDefinationTest.java

--- src/test/java/parallel/searchstepDefinationTest.java
+++ src/test/java/parallel/searchstepDefinationTest.java
@@ -7,7 +7,7 @@ import org.openqa.selenium.By;
 import io.cucumber.java.en.And;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
-import jdk.internal.org.jline.utils.Log;
+
 import pageObjects.search;
 import resources.Base;
 
